dd39017cadeb115c478b6abf61384bc63909f9aa
Muhammad Nouman <nomannisar769@gmail.com>
2026-04-06 17:13:56 +0500

    feat: add introductory scripts for JavaScript variables and data types

---
+++ JS/01_variables.js
@@ -0,0 +1,25 @@
+const accountId = 1;
+let accountEmail = "nomannisar769@gmail.com";
+var accountPassword = "@Usama22";
+accountCity = "Farooqabad";
+let accountState;
+
+// accountId = 2; not allowed because const is used
+
+// accountEmail = "kbgf v"
+// accountPassword = "756756"
+// accountCity = "Lahore"
+// accountPassword = "64"
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+/*
+prefer not to use var 
+because of block scope and functional scope
+
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ JS/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using node js not browser
+
+// code should be readable
+// code readability is exactly what we want:
+// like this:
+// console.log(3 
+//     + 3); // not like this:
+// like this:
+// console.log(3 + 3);
+
+/// data types in js:
+// number => 2 to power 53 like 2**53
+// bigint => for larger numbers like 2**53 + 1
+// string => "hello" or 'hello' or `hello`
+// boolean => true/false (only two values)
+// null => standalone value (it is an object)  
+// undefined => value is not assigned (it is an object)
+// symbol => unique value (it is an object)
+// object => {} (it is an object)    
+
+let name = "Usama";
+let age = 22;
+let isLoggedIn = false;
+
+console.log(typeof null); // object interview question) what is null in js answer is object but it is a bug in js
+console.log(typeof undefined); // undefined  
+
