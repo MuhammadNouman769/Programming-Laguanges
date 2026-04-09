9bc8a454c1ed9e2e3af17e3008746422b1a711e3
Muhammad Nouman <nomannisar769@gmail.com>
2026-04-09 12:24:09 +0500

    conversion oprations

--- JS/03_conversionOpration.js
+++ JS/03_conversionOpration.js
@@ -1,4 +1,4 @@
-let score = "33"
+let score = true;
 
 console.log(typeof score);
 console.log(typeof (score));
@@ -6,3 +6,25 @@ console.log(typeof (score));
 let valueInNumber = Number(score)
 console.log(valueInNumber);
 console.log(typeof valueInNumber);
+
+// // "33" => 33
+// // "33abc" => NaN
+// // true => 1
+// // false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "abc" => true
+// null => false
+// undefined => false
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
