ea3071edab7b4f95c96f5c8e3d29797f73b474d7
Muhammad Nouman <nomannisar769@gmail.com>
2026-06-09 17:48:28 +0500

    Remove outdated Python function practice files related to inheritance types, including single, multiple, multilevel, hierarchical, diamond problem, and MRO. Clean up workspace settings accordingly.

---
+++ python/python_function_practice/class_01/list/list.py
@@ -0,0 +1,62 @@
+
+""" ================ List =============== """
+'''
+
+1. List kia hai 
+Answer: list aik order mutable collection hai 
+Example.i
+
+'''
+names = ["Ali", "Ahmad", "Usama"]
+print(names)
+
+'''
+list ki properties:
+      i. order
+     ii. Mutable
+    iii. duplicate allowed
+     iv. defferent data types store
+
+list create karne ka tarika
+'''
+a = [1, 2, 3]
+
+'''
+2.append() method kia hai
+Answer: End pe aik item add krta hai
+
+Example:i
+
+'''
+nums = [1, 2, 3]
+nums.append(4)
+print(nums)
+
+'''
+3. extend() method kia hai
+Answer: extend() method multiple items add krta hai
+
+Example:i
+'''
+number = [1, 2]
+number.extend([3, 4])
+print(number)
+
+'''
+4. insert method kia hai
+Answer: insert() method specific index par add krta hai 
+
+Example.i
+'''
+a = [1, 3, 4]
+a.insert(1, 2)
+
+'''
+5. remove() method kia krta hai 
+Answer: value remov krta hai
+
+Example.i
+'''
+a = [1, 2, 3]
+a.remove(3)
+print(a)
