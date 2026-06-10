55adfe8927fb26b8fbc2b9d3587a6920de64c852
Muhammad Nouman <nomannisar769@gmail.com>
2026-06-10 15:21:49 +0500

    Add new Python practice file for list operations, including examples for adding, removing, reversing, and finding maximum values in lists. Update existing list.py with additional methods and explanations for pop, clear, index, count, sort, and reverse.

--- python/python_function_practice/class_01/list/list.py
+++ python/python_function_practice/class_01/list/list.py
@@ -7,6 +7,9 @@ Answer: list aik order mutable collection hai
 Example.i
 
 '''
+from curses import has_ic
+
+
 names = ["Ali", "Ahmad", "Usama"]
 print(names)
 
@@ -60,3 +63,149 @@ Example.i
 a = [1, 2, 3]
 a.remove(3)
 print(a)
+
+'''
+6. pop() method kia hai 
+Answer: index se remove krta hai or value return krta hai 
+Example.i
+'''
+a = [1, 2, 3, 4]
+
+x = a.pop(0)
+print(x)
+
+''' 
+spacific index remove kr dain ga
+'''
+
+a.pop(0)
+print(a)
+
+'''
+7. clear() method kia krta hai 
+Answer: list empty kr deta hai
+Example.i
+'''
+a = [1, 2, 3, 4]
+
+a.clear()
+print(a)
+
+'''
+8. index() method kia krta hai
+Answer: position btata hai
+
+Example.i
+'''
+a = [1, 2, 3]
+
+print(a.index(1))
+
+'''
+9. count() method kia krta hai
+Answer: kitni dafa item aya
+
+Example.i
+'''
+
+a = [1, 2, 3, 3, 3, 4]
+
+print(a.count(3))
+
+'''
+10. sort() or sorted() method kia krta hai 
+ascending 
+descending
+Example.i
+'''
+
+a = [4, 3, 2 ,1]
+
+a.sort()
+print(a)
+
+a.sort(reverse=True)
+print(a)
+
+
+
+a = [4, 2, 1]
+
+b = sorted(a)
+
+print(a)
+print(b)
+
+
+'''
+Interview Question
+
+sort()         vs             sorted()
+
+sort()                  	sorted()
+Original list change	      New list return
+List method	                  Built-in function
+
+
+11. reverse() method kia hai 
+Answer: list ko ult krta hai 
+
+Example.i
+'''
+
+a = [1, 2, 3]
+
+a.reverse()
+print(a)
+
+
+'''
+12.join() ki hai
+
+Answer: join list method nahi hai. Ye string method hai
+ye do string ko jorta hai
+Example.i
+'''
+names = ["Ali", "Ahmed", "Usama"]
+
+result = ",".join(names)
+
+print(result)
+
+'''
+13. split() method kia hai 
+Answer: String ko list me convert karta hai.
+Interview Question
+
+split aur join opposite hain?
+
+Han
+Example.i
+'''
+data = "Ali Ahmed Usama"
+
+result = data.split()
+
+print(result)
+
+'''
+slicing
+'''
+a = [1, 3, 4, 5, 6, 7]
+
+print(a[-1])
+print(a[1:3])
+print(a[1])
+print(a[::-1])
+
+'''
+nested list
+'''
+
+
+data = [
+    [1, 2],
+    [3, 4]
+]
+
+print(data[1][0])
---
+++ python/python_function_practice/class_01/list/list_practice.py
@@ -0,0 +1,19 @@
+'''
+Practice Questions
+    Easy
+    i.List me 100 add karo.
+   ii.Last element remove karo.
+  iii.First element remove karo.
+   iv.List reverse karo.
+    v.Maximum number find karo.
+'''    
+
+list1 = []
+list1.extend([10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+print(list1)
+
+list2 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+list2.remove(100)
+list2.remove(10)
+list2.reverse()
+print(list2)
