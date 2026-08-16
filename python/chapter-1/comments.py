c13c09e661d2f13448febd2402e589046d19e988
Muhammad Nouman <noumannisar769@gmail.com>
2026-08-16 13:07:05 +0500

    Add comments and variables explanations in Python chapter 1

---
+++ python/chapter-1/comments.py
@@ -0,0 +1,20 @@
+'''
+1.what is comments in python ?
+
+ans: comments are notes you write in your code for yourself 
+     (or other developers). python completety ignore them
+'''
+# example.1:
+
+# this is my name <--- comment
+name1 = 'Nouman'
+
+# example.2:
+# this is my son name <--- comment
+name2 = 'Usama Nouman'
+
+# example.3:
+# this is my wife name <--- comment
+name3 = 'Shanzy'
+
+print(f' my name is {name1} my wife name is {name3} and our child name is {name2}')
---
+++ python/chapter-1/variables.py
@@ -0,0 +1,17 @@
+'''
+2 what is variable ?
+ans: variable aik named memory location hota hai jo temparery data store krne k lia use hota ha 
+
+Rules
+1name = --> can't start with a number
+my name --> no sapces allowed
+my-name = no special charactors (except underscore)
+'''
+# examples
+name = 'Nouman' # name is a variable
+age = 27 # age is variable
+adress = 'Farooqabad' # adress is variable
+children = 1 # children is a vaiable
+
+#camel case
+myName = 'Nouman'
